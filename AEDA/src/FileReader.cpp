e1eb6a362be0b2805964dd694f87a0689e4c6035
jkkjojoml <ricardoboia34@hotmail.com>
2017-11-08 14:27:46 +0000

    Reads clients file

--- AEDA/src/FileReader.cpp
+++ AEDA/src/FileReader.cpp
@@ -15,32 +15,31 @@ using namespace std;
 
 std::vector<std::string> FileReader::readLines(std::string file) {
 	std::vector<std::string> lines;
-	cout << "fdfd";
+	cout << file;
 	ifstream inFile(file);
 	if (inFile.is_open())
 	{
-		cout << "ff" ;
 		std::string line;
 
-		while (getline(inFile, line))
+		while (getline(inFile, line)) {
 			lines.push_back(line);
+		}
 
 		inFile.close();
 	}
+
 	return lines;
 }
 
 std::vector<Client *> FileReader::readClients(std::string file)
 {
 	std::vector<Client *> clients;
-	cout << "lll";
 	std::vector<std::string> lines = FileReader::readLines(file);
+
 	for (unsigned int i = 0; i < lines.size(); i++)
 	{
-		cout << "e";
 		std::string line = lines[i];
-		std::cout << "3";
-		std::string name, NIF=0;
+		std::string name, NIF;
 
 		line = line.substr(line.find(',') + 2);
 		name = line.substr(0,line.find(','));
@@ -49,9 +48,9 @@ std::vector<Client *> FileReader::readClients(std::string file)
 
 		Client *c = new Client(name, std::stoi(NIF.c_str()));
 		clients.push_back(c);
-		std::cout << "4";
 	}
-	std::cout << "5";
+
+
 	return clients;
 }
 
--- AEDA/src/Main.cpp
+++ AEDA/src/Main.cpp
@@ -17,7 +17,7 @@
 
 using namespace std;
 
-#define clientsFile "C:\\Users\\ricar\\git\\AEDA17-18\\AEDA\\Debug\\clients.txt"
+#define clientsFile "clients.txt"
 #define suppliersFile "suppliers.txt"
 #define offersFile "offers.txt"
 
@@ -27,21 +27,10 @@ int main()
 	std::vector <Supplier *> suppliers;
 	std::vector <Offer *> offers;
 
-	RegisteredClient* c1 = new RegisteredClient("joao",0);
-	clients.push_back(c1);
-
 	Company c(clients,suppliers,offers);
 
-	//c.printClients();
 
-	for (unsigned int i = 0; i < clients.size(); i++)
-		{
-			std::cout << clients[i]->getID() << " " <<
-					     clients[i]->getName() << " " <<
-						 clients[i]->getNIF() << " " << std::endl;
-		}
-
-	//CompanyMenu (&c);
+	CompanyMenu (&c);
 
 	return 0;
 }
